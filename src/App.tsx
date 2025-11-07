fc3aa439436295f3c73741af92e77796d9a2d880
kaziabulhasib <hasibkazi420@gmail.com>
2025-11-07 11:13:06 +0530

    add custom colors, spacing, and radius

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 function App() {
   return (
     <div>
-      <h1 className='text-red-500 text-5xl'>hello world</h1>
+      <h1 className='text-primary-600 text-5xl'>hello world</h1>
+      <h1 className='text-neutral-500'>hello world</h1>
     </div>
   );
 }
